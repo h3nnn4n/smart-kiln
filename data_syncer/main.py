309e8d5f9c00155196e824cd750546e2362023b0
h3nnn4n <git@h3nnn4n.me>
2023-01-22 16:15:35 -0300

    Setup simple reader

--- data_syncer/main.py
+++ data_syncer/main.py
@@ -1,6 +1,23 @@
+import serial
+from datetime import datetime
+
+PORT = "/dev/ttyUSB0"
+BAUDRATE = 9600
+TIMEOUT = 2
+
+
 def main():
-    pass
+    conn = serial.Serial(port=PORT, baudrate=BAUDRATE, timeout=TIMEOUT)
+    print(f"connected to port {conn.name}")
+
+    while True:
+        data = conn.readline().strip().decode()
+        now = datetime.now()
+        now_str = now.strftime("%H:%M:%S")
+        print(f"{now_str} {data}")
+
+    conn.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
