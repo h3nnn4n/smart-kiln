232790e0cc3f1ff5654b29253b416ad4ff85240a
h3nnn4n <git@h3nnn4n.me>
2023-01-22 20:16:46 -0300

    Restart after a set amount of readings

--- data_syncer/config.py
+++ data_syncer/config.py
@@ -5,6 +5,8 @@ PORT = config("PORT")
 BAUDRATE = config("BAUDRATE", default=9600, cast=int)
 TIMEOUT = config("TIMEOUT", default=2, cast=int)
 
+RESET_AFTER_N_MEASUREMENTS = config("RESET_AFTER_N_MEASUREMENTS", default=30, cast=int)
+
 INFLUXDB_HOST = config("INFLUXDB_HOST")
 INFLUXDB_PORT = config("INFLUXDB_PORT")
 INFLUXDB_USER = config("INFLUXDB_USER")
--- data_syncer/main.py
+++ data_syncer/main.py
@@ -2,18 +2,19 @@ from datetime import datetime
 
 import serial
 
+import config
 from config import BAUDRATE, PORT, TIMEOUT
-
 from metrics import push_measurement
 
 
-def main():
+def loop():
     conn = serial.Serial(port=PORT, baudrate=BAUDRATE, timeout=TIMEOUT)
     print(f"connected to port {conn.name}")
 
     last_read_id = None
+    measurements_taken = 0
 
-    while True:
+    while measurements_taken < config.RESET_AFTER_N_MEASUREMENTS:
         data = conn.readline().strip().decode()
 
         if not data:
@@ -28,7 +29,7 @@ def main():
 
         now = datetime.now()
         now_str = now.strftime("%H:%M:%S")
-        print(f"{now_str} {read_id} {temperatures}")
+        print(f"{now_str} {measurements_taken:2}/{config.RESET_AFTER_N_MEASUREMENTS:2} {read_id} {temperatures}")
 
         for sensor_id, temperature in enumerate(temperatures):
             push_measurement(
@@ -39,7 +40,17 @@ def main():
                 },
             )
 
+        measurements_taken += 1
+
+    if measurements_taken >= config.RESET_AFTER_N_MEASUREMENTS:
+        print(f"took {measurements_taken} measurements. Limit is {config.RESET_AFTER_N_MEASUREMENTS}")
+
     conn.close()
+
+
+def main():
+    while True:
+        loop()
 
 
 if __name__ == "__main__":
