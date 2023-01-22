fe2172ea2765b5f5cf9fb8a55d487a7a46997fc9
h3nnn4n <git@h3nnn4n.me>
2023-01-22 16:35:20 -0300

    Orange pi gpio test

---
+++ data_syncer/gpio.py
@@ -0,0 +1,13 @@
+import orangepi.pi3
+from OPi import GPIO
+
+
+channel = 3
+
+GPIO.setmode(orangepi.pi3.BOARD)
+GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+if GPIO.input(channel):
+    print('Input was HIGH')
+else:
+    print('Input was LOW')
