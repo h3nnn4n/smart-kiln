a6ef02687297f29064eabb6de3146d1f6bedac70
h3nnn4n <git@h3nnn4n.me>
2023-04-09 20:59:40 -0300

    Setup a 4th temp sensor

--- smart_kiln/src/main.cpp
+++ smart_kiln/src/main.cpp
@@ -9,6 +9,7 @@ const int common_DO = 5;
 const int sensor1_CS = 6;
 const int sensor2_CS = 7;
 const int sensor3_CS = 8;
+const int sensor4_CS = 9;
 
 // How long we wait before reading the sensors after each loop
 const int LOOP_INTERVAL = 1;  // Seconds
@@ -18,6 +19,7 @@ unsigned int counter = 0;
 MAX6675 sensor1(common_CLK, sensor1_CS, common_DO);
 MAX6675 sensor2(common_CLK, sensor2_CS, common_DO);
 MAX6675 sensor3(common_CLK, sensor3_CS, common_DO);
+MAX6675 sensor4(common_CLK, sensor4_CS, common_DO);
 
 void setup() {
     Serial.begin(9600);
@@ -31,6 +33,7 @@ void loop() {
     float t1 = sensor1.readCelsius();
     float t2 = sensor2.readCelsius();
     float t3 = sensor3.readCelsius();
+    float t4 = sensor4.readCelsius();
 
     Serial.print(counter);
     Serial.print(",");
@@ -39,6 +42,8 @@ void loop() {
     Serial.print(t2);
     Serial.print(",");
     Serial.print(t3);
+    Serial.print(",");
+    Serial.print(t4);
     Serial.println();
 
     counter++;
