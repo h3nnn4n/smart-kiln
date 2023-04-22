162af3f90dd6ebc691730c7c46e1a818640c1386
h3nnn4n <git@h3nnn4n.me>
2023-04-22 02:04:58 -0300

    Remove blocking calls

--- smart_kiln/src/main.cpp
+++ smart_kiln/src/main.cpp
@@ -15,8 +15,13 @@ const int sensor4_CS = 9;
 // The Solid State Relay pin
 const int SSR = 3;
 
-// How long we wait before reading the sensors after each loop
-const int LOOP_INTERVAL = 1;  // Seconds
+// How long we wait before sending the data to the logger
+const int SERIAL_OUT_INTERVAL = 1 * 1000;
+unsigned long serial_out_timer = 0;
+
+// How long we wait before reading the sensors
+const int SENSOR_READ_INTERVAL = 1 * 250;
+unsigned long sensor_read_timer = 0;
 
 unsigned int counter = 0;
 
@@ -27,6 +32,11 @@ double pid_kp = 7.5f;
 double pid_ki = 0.0f;
 double pid_kd = 0.0f;
 
+float t1 = 0;
+float t2 = 0;
+float t3 = 0;
+
+
 PID pid(&pid_input, &pid_output, &pid_setpoint, pid_kp, pid_ki, pid_kd, 0);
 
 MAX6675 sensor1(common_CLK, sensor1_CS, common_DO);
@@ -34,6 +44,9 @@ MAX6675 sensor2(common_CLK, sensor2_CS, common_DO);
 MAX6675 sensor3(common_CLK, sensor3_CS, common_DO);
 
 void setup() {
+	serial_out_timer = millis();
+	sensor_read_timer = millis();
+
     Serial.begin(9600);
 
     Serial.println("Smart kiln init");
@@ -50,25 +63,33 @@ void setup() {
 }
 
 void loop() {
-	float t1 = sensor1.readCelsius();
-	float t2 = sensor2.readCelsius();
-	float t3 = sensor3.readCelsius();
+	unsigned long now = millis();
 
-	pid_input = t1;
+	if ((now - sensor_read_timer) > SENSOR_READ_INTERVAL) {
+		t1 = sensor1.readCelsius();
+
+		pid_input = t1;
+		sensor_read_timer = now;
+	}
 
 	if (pid.Compute()) {
 		analogWrite(SSR, pid_output);
 	}
 
-    Serial.print(counter);
-    Serial.print(",");
-    Serial.print(t1);
-	Serial.print(",");
-	Serial.print(pid_input);
-	Serial.print(",");
-	Serial.print(pid_output);
-    Serial.println();
+	if ((now - serial_out_timer) > SERIAL_OUT_INTERVAL) {
+		t2 = sensor2.readCelsius();
+		t3 = sensor3.readCelsius();
 
-    counter++;
-    delay(LOOP_INTERVAL * 1000);
+		Serial.print(counter);
+		Serial.print(",");
+		Serial.print(t1);
+		Serial.print(",");
+		Serial.print(t2);
+		Serial.print(",");
+		Serial.print(pid_output);
+		Serial.println();
+
+		counter++;
+		serial_out_timer = now;
+	}
 }
