91d466f4488e3a125b9e0cd3df53f1fe6dc2515c
h3nnn4n <git@renan.digital>
2023-04-29 09:02:34 -0300

    Simple firing schedule program using linear interpolation

--- data_syncer/program.py
+++ data_syncer/program.py
@@ -1,3 +1,4 @@
+import itertools
 from datetime import datetime, timedelta
 
 
@@ -5,12 +6,56 @@ PROGRAM_UPDATE_INTERVAL = timedelta(seconds=60)
 
 
 class ProgramState:
-    def __init__(self):
+    def __init__(self, program):
+        self.program = program
+
+        # These are actual timestamps
         self.start_time = datetime.now()
+        self.end_time = self._calculate_end_time()
+
+        # These are timedeltas
+        self.program_timer = timedelta(seconds=0)
+        self.end_time_offset = self._calculate_end_offset()
+
+        for k, v in self.program.items():
+            print(f"{k} {v}")
+
+    def _calculate_end_offset(self):
+        return max(list(self.program.keys()))
+
+    def _calculate_end_time(self):
+        end_time_offset = self._calculate_end_offset()
+        end_time = self.start_time + end_time_offset
+        return end_time
+
+    def run(self):
+        now = self.start_time
+
+        while now < self.end_time:
+            now += PROGRAM_UPDATE_INTERVAL
+            self.program_timer += PROGRAM_UPDATE_INTERVAL
+            target_temp = self._calculate_temperature(self.program_timer)
+            print(f"{now=} {target_temp=}")
+
+    def _calculate_temperature(self, time: timedelta) -> float:
+        for time1, time2 in itertools.pairwise(self.program.keys()):
+            if time1 < time and time < time2:
+                break
+
+        temp1 = self.program[time1]
+        temp2 = self.program[time2]
+
+        # Simple linear interpolation
+        p: float = (time - time1) / (time2 - time1)
+        temp: float = temp1 + (temp2 - temp1) * p
+
+        return temp
 
 
 def main():
-    read_program()
+    program = read_program()
+    program_state = ProgramState(program)
+    program_state.run()
 
 
 def read_program():
@@ -27,8 +72,7 @@ def read_program():
 
             temps[time_offset] = temp
 
-    for k, v in temps.items():
-        print(f"{k} {v}")
+    return temps
 
 
 if __name__ == "__main__":
